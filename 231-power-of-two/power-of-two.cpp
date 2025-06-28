576e5ae3d04f409d24d58333556858d16d3035a5
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-06-29 00:59:41 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.77%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -3,4 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         return n > 0 && (n & (n - 1)) == 0;
     }
+
+    bool isPowerOfFour(int n) {
+        return isPowerOfTwo(n) && (n & 0xAAAAAAAA) == 0;
+    }
 };
