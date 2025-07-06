3d72a57a29618e61935d9b8bfb332493d86201b9
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-07-06 12:15:54 +0530

    Time: 1 ms (33.29%) | Memory: 9.1 MB (5.13%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool isHappy(int n) {
         int total = n;
-        for (int j = 0; j < 100; j++) {
+        for (int j = 0; j < 50; j++) {
             string s = to_string(total);
-            int sum = 0;  // Reset sum for each iteration
+            int sum = 0;  
             for (int i = 0; i < s.size(); i++) {
                 char ch = s[i];
                 int num = ch - '0';
