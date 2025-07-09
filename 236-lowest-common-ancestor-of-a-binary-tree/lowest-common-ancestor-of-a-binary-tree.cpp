934e96c26044a2fa4be53ed6ddacefa3f03de91c
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-07-09 11:05:42 +0530

    Time: 311 ms (5.06%) | Memory: 17.4 MB (69.14%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -4,7 +4,7 @@ private:
         if (root == NULL) return false;
         if (root == target) return true;
 
-        // Check in left or right subtree
+        
         return solve(root->left, target) || solve(root->right, target);
     }
 
@@ -12,20 +12,20 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (root == NULL) return NULL;
 
-        // If root is p or q, return root
+        
         if (root == p || root == q) return root;
 
         bool isPInLeft = solve(root->left, p);
         bool isQInLeft = solve(root->left, q);
 
         if (isPInLeft && isQInLeft) {
-            // Both in left subtree
+           
             return lowestCommonAncestor(root->left, p, q);
         } else if (!isPInLeft && !isQInLeft) {
-            // Both in right subtree
+            
             return lowestCommonAncestor(root->right, p, q);
         } else {
-            // One in left, one in right => current node is LCA
+          
             return root;
         }
     }
