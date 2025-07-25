bb346a8fb0b985d497ccecfb6c47ea8d8cf6bf32
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-07-25 09:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (86.99%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0 && mp.find(nums[i]) == mp.end()) {
+                mp[nums[i]]++;
+                sum = sum + nums[i];
+            }
+        }
+        int maxi=INT_MIN;
+        if(sum==0){
+            for(int i=0;i<n;i++){
+              int val=nums[i];
+              maxi=max(maxi,nums[i]);
+            }
+            return maxi;
+        }
+     return sum;
+    }
+};
