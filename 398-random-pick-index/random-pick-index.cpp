9a6f2c9e6d165b029c73667aab45c6208c09835f
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-08-02 18:12:48 +0530

    Time: 36 ms (88.33%) | Memory: 66.9 MB (37.57%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    unordered_map<int, vector<int>> mp; 
+public:
+    Solution(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            mp[nums[i]].push_back(i);
+        }
+    }
+    
+    int pick(int target) {
+        vector<int>& indices = mp[target];
+        int randomIndex = rand() % indices.size(); 
+        return indices[randomIndex];
+    }
+};
