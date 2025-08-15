6deed08f91317d5f2694259d9b93ebb0ca343271
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-08-15 13:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (60.46%) - LeetSync

--- 397-integer-replacement/integer-replacement.cpp
+++ 397-integer-replacement/integer-replacement.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int integerReplacement(int n) {
-        long long N = n; // avoid overflow
+        long long N = n; 
         int steps = 0;
         
         while (N > 1) {
             if (N % 2 == 0) {
-                N >>= 1; // divide by 2
+                N >>= 1; 
             } else {
                 if (N == 3 || ((N >> 1) & 1) == 0) {
-                    N--; // go down
+                    N--; 
                 } else {
-                    N++; // go up
+                    N++; 
                 }
             }
             steps++;
