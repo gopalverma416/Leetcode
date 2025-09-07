913e893b9f56ac381d9f828d8d42726d1127c587
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-09-07 18:44:03 +0530

    Time: 8 ms (18.74%) | Memory: 19.2 MB (19.09%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& f : flights) {
-            adj[f[0]].emplace_back(f[1], f[2]);
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto& e : flights) {
+            adj[e[0]].push_back({e[1], e[2]});
         }
 
-        // Min-heap: (cost, node, stops)
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        pq.emplace(0, src, 0);
+        // dist[node][stops] = min cost
+        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
 
-        // Track visited with stops
-        vector<int> stopsToNode(n, INT_MAX);
+        pq.push({0, src, 0});
+        dist[src][0] = 0;
 
         while (!pq.empty()) {
-            auto [cost, u, stops] = pq.top();
+            auto [cost, node, stops] = pq.top();
             pq.pop();
 
-            // ✅ Reached destination
-            if (u == dst) return cost;
+            if (node == dst) return cost;
+            if (stops > k) continue;
 
-          
-            if (stops > k || stops >= stopsToNode[u]) continue;
-
-            stopsToNode[u] = stops;
-
-            for (auto& [v, price] : adj[u]) {
-                pq.emplace(cost + price, v, stops + 1);
+            for (auto& [nei, price] : adj[node]) {
+                int newCost = cost + price;
+                if (newCost < dist[nei][stops + 1]) {
+                    dist[nei][stops + 1] = newCost;
+                    pq.push({newCost, nei, stops + 1});
+                }
             }
         }
-
         return -1;
     }
 };
