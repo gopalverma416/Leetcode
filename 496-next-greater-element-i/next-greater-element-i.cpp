073fef6c2c9f87e2807ba522f316e435876edd67
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-09-28 17:46:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (81.13%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> mp;  
-        int n = nums2.size();
-
-        
-        for (int i = 0; i < n; i++) {
-            mp[nums2[i]] = i;
-        }
-
-        stack<int> st;
-        vector<int> next(n, -1);
-
-       
-        for (int i = n - 1; i >= 0; i--) {
-            int val = nums2[i];
-
-           
-            while (!st.empty() && st.top() <= val) {
+        int n=nums1.size();
+        int m=nums2.size();
+        stack<int>st;
+        vector<int>res(m,-1);
+        for(int i=m-1;i>=0;i--){
+            int val=nums2[i];
+            while(!st.empty() && st.top()<=val){
                 st.pop();
             }
-
-          
-            if (!st.empty()) {
-                next[i] = st.top();
+            if(!st.empty()){
+                res[i]=st.top();
             }
-
-           
             st.push(val);
         }
-
-        
-        vector<int> res(nums1.size());
-        for (int i = 0; i < nums1.size(); i++) {
-            int idx = mp[nums1[i]];
-            res[i] = next[idx];
-        }
-
-        return res;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int val1=nums1[i];
+                int val2=nums2[j];
+                if(val1==val2){
+                    nums1[i]=j;
+                    break;
+                }
+            }
+         }
+         vector<int>ans;
+         for(int i=0;i<n;i++){
+           int idx=nums1[i];
+           ans.push_back(res[idx]);
+         }
+      return ans;
     }
 };
