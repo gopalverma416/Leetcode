82fba7dc6fcc9a910d0af2f8f444c46a801f9766
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-04 19:29:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (91.83%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -10,28 +10,22 @@
  * };
  */
 class Solution {
-    private:
-    unordered_map<TreeNode*,int>mp;
-    int solve(TreeNode* root){
-        if(!root){
-            return 0;
-        }
-        if(mp.count(root)){
-            return mp[root];
-        }
-        int take=root->val;
-        if(root->left){
-            take+=solve(root->left->left)+solve(root->left->right);
-        }
-        if(root->right){
-         take+=solve(root->right->left)+solve(root->right->right);
-        }
-        int nonTake=0;
-        nonTake=solve(root->left)+solve(root->right);
-        return mp[root]=max(nonTake,take);
-    }
 public:
+    pair<int,int> solve(TreeNode* root){
+         if(!root){
+            return {0,0};
+         }
+         auto left=solve(root->left);
+         auto right=solve(root->right);
+
+         int take=0;
+         take=root->val+left.second+right.second;
+         int nonTake=max(left.first,left.second)+max(right.first,right.second);
+         return {take,nonTake};
+
+    }
     int rob(TreeNode* root) {
-        return solve(root);
+        auto temp= solve(root);
+        return max(temp.first,temp.second);
     }
 };
