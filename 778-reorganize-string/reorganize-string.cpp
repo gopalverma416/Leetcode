bf8b1498ee293f5ef2babfa4823ebfa66a2ead87
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-13 22:33:52 +0530

    Time: 3 ms (13.19%) | Memory: 8.4 MB (72.18%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char, int> mp;
-        int n = s.size();
-
-        for (char ch : s) {
-            mp[ch]++;
-            if (mp[ch] > (n + 1) / 2) return "";  
+        int n=s.size();
+        unordered_map<char,int>mp;
+        for(auto t:s){
+         mp[t]++;
         }
-
-       
-        priority_queue<pair<int, char>> pq;
-        for (auto& [ch, freq] : mp) {
-            pq.push({freq, ch});
+        priority_queue<pair<int,char>>pq;
+        for(auto m:mp){
+            char first=m.first;
+            int second=m.second;
+            pq.push({second,first});
         }
-
-        string ans = "";
-        while (pq.size() >= 2) {
-            auto [freq1, ch1] = pq.top(); pq.pop();
-            auto [freq2, ch2] = pq.top(); pq.pop();
-
-            ans += ch1;
-            ans += ch2;
-
-            if (--freq1 > 0) pq.push({freq1, ch1});
-            if (--freq2 > 0) pq.push({freq2, ch2});
+        string ans;
+        while(pq.size()>1){
+            auto [freq1,ch1]=pq.top();
+            pq.pop();
+            auto [freq2,ch2]=pq.top();
+            pq.pop();
+            ans+=ch1;
+            ans+=ch2;
+            if(freq1-1>0){
+                pq.push({freq1-1,ch1});
+            }
+            if(freq2-1>0){
+                 pq.push({freq2-1,ch2});
+            }
         }
-
-        if (!pq.empty()) {
-            if (pq.top().first > 1) return "";  
-            ans += pq.top().second;
+        while(!pq.empty()){
+            auto [frq3,ch3]=pq.top();
+            pq.pop();
+            if(frq3>1){
+                return "";
+            }else{
+                ans+=ch3;
+            }
         }
-
         return ans;
     }
 };
