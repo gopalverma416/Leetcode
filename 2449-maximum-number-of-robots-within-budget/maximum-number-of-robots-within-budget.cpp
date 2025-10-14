55bebbd48936ecdeff2ca7346e10cda14588a890
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-14 09:13:56 +0530

    Time: 113 ms (46.40%) | Memory: 119.6 MB (58.97%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
@@ -1,49 +1,49 @@
 class Solution {
-public:
-    long long maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        int n = chargeTimes.size();
-        vector<long long> prefix(n, 0);
-        prefix[0] = runningCosts[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + runningCosts[i];
+    private:
+    bool solve(vector<int>& charge,vector<int>& run,long long budget,int k){
+        deque<int>dq;
+        long long sum=0;
+        int n=charge.size();
+        for(int i=0;i<n;i++){
+            sum+=run[i];
+            while(!dq.empty() && charge[dq.back()]<=charge[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k){
+                sum-=run[i-k];
+                if(dq.front()==i-k){
+                    dq.pop_front();
+                }
+            }
+            if(i>=k-1){
+                long long total=charge[dq.front()]+1LL*k*sum;
+                if(total<=budget){
+                    return true;
+                }
+            }
+    
         }
+        return false;
 
-        int left = 1, right = n;
-        int ans = 0;
-
-        while(left <= right){
-            int k = left + (right-left)/2;
-            if(canFit(chargeTimes, prefix, budget, k)){
-                ans = k;
-                left = k + 1;
-            } else {
-                right = k - 1;
+       
+    }
+public:
+    
+    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
+        int n=chargeTimes.size();
+        int low=1;
+        int high=n;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(solve(chargeTimes,runningCosts,budget,mid)){
+              ans=mid;
+              low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
         return ans;
-    }
-
-private:
-    bool canFit(vector<int>& chargeTimes, vector<long long>& prefix, long long budget, int k){
-        deque<int> dq;
-        int n = chargeTimes.size();
-
-        for(int i=0; i<n; i++){
-        
-            while(!dq.empty() && chargeTimes[dq.back()] <= chargeTimes[i])
-                dq.pop_back();
-            dq.push_back(i);
-
-           
-            if(dq.front() <= i-k)
-                dq.pop_front();
-
-            if(i >= k-1){
-                long long sumRunning = prefix[i] - (i>=k ? prefix[i-k] : 0);
-                long long totalCost = chargeTimes[dq.front()] + sumRunning * k;
-                if(totalCost <= budget) return true;
-            }
-        }
-        return false;
     }
 };
