a3e24509a57aaa8430fd32c8455b3ea4b17ae298
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-16 15:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (68.03%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -1,32 +1,30 @@
-
 class Solution {
     private:
-    int camera=0;
-    int dfs(TreeNode* root){
-        if(!root){
+    int dfs(TreeNode* root,int& cnt){
+        if(root==NULL){
             return 1;
         }
-        // 2->node need camera
-        // 1->node is covered by camera
-        // 0->camera is placed at the node
-        int left=dfs(root->left);
-        int right=dfs(root->right);
-        if(left==2 || right==2){
-            camera++;
+        // 2->root->node need camera 
+        // 1->camera by camera
+        // 0->has camera 
+        int left=dfs(root->left,cnt);
+        int right=dfs(root->right,cnt);
+        if(left==2||right==2){
+            cnt++;
             return 0;
         }
-        if(left==0 ||right==0){
+        if(left==0||right==0){
             return 1;
         }
-        return 2;
+       return 2;
+
     }
-    
 public:
     int minCameraCover(TreeNode* root) {
-        if(dfs(root)==2){
-            camera++;
-            return camera;
+        int cnt=0;
+        if(dfs(root,cnt)==2){
+            cnt++;
         }
-        return camera;
+     return cnt;
     }
 };
