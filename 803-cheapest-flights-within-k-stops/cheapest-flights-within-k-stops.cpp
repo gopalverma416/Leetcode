b27d53ad607410323b3a245bd10005a4f3a8a3e6
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-18 00:22:13 +0530

    Time: 3 ms (63.17%) | Memory: 19.1 MB (20.15%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto& e : flights) {
-            adj[e[0]].push_back({e[1], e[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        int sz = flights.size();
+        for (int i = 0; i < sz; i++) {
+            int u = flights[i][0];
+            int v = flights[i][1];
+            int wt = flights[i][2];
+            adj[u].push_back({v, wt});
         }
-
-        // dist[node][stops] = min cost
-        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
-        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
-
+        using P = tuple<int, int, int>;
+        priority_queue<P, vector<P>, greater<P>> pq;
         pq.push({0, src, 0});
-        dist[src][0] = 0;
-
+        vector<vector<int>> dist(n + 1, vector<int>(k + 2, INT_MAX));
         while (!pq.empty()) {
-            auto [cost, node, stops] = pq.top();
+            auto [step, node, stop] = pq.top();
             pq.pop();
-
-            if (node == dst) return cost;
-            if (stops > k) continue;
-
-            for (auto& [nei, price] : adj[node]) {
-                int newCost = cost + price;
-                if (newCost < dist[nei][stops + 1]) {
-                    dist[nei][stops + 1] = newCost;
-                    pq.push({newCost, nei, stops + 1});
+             if (node == dst) {
+                return step;
+            }
+            if (stop > k) {
+                continue;
+            }
+           
+            for (auto [neig, wt] : adj[node]) {
+                int newSum = step + wt;
+                if (newSum < dist[neig][stop + 1]) {
+                    dist[neig][stop + 1] = newSum;
+                    pq.push({newSum, neig, stop + 1});
                 }
             }
         }
