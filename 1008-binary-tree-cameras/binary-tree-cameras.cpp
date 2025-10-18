c44901cfca6c963733c94485602dc0b26eefd16e
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-18 18:21:13 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (39.82%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -1,30 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
     private:
-    int dfs(TreeNode* root,int& cnt){
-        if(root==NULL){
-            return 1;
-        }
-        // 2->root->node need camera 
-        // 1->camera by camera
-        // 0->has camera 
-        int left=dfs(root->left,cnt);
-        int right=dfs(root->right,cnt);
-        if(left==2||right==2){
-            cnt++;
-            return 0;
-        }
+    int camera;
+    int dfs(TreeNode* root){
+        // 2->need camera
+        // 1->node covered by camera
+        //  0->node have camera
+       if(!root){
+         return 1;
+       }
+       int left=dfs(root->left);
+       int right=dfs(root->right);
+       
+       if(left==2 || right==2){
+        camera++;
+        return 0;
+       }
         if(left==0||right==0){
-            return 1;
-        }
+          return 1;
+       }
        return 2;
-
     }
 public:
     int minCameraCover(TreeNode* root) {
-        int cnt=0;
-        if(dfs(root,cnt)==2){
-            cnt++;
+        camera=0;
+        if(dfs(root)==2){
+            camera++;
         }
-     return cnt;
+        return camera;
     }
 };
