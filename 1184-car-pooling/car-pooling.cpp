5f644cd206bc6d0560b538f906d355b546b63bf7
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-10-22 10:50:13 +0530

    Time: 6 ms (16.32%) | Memory: 14.1 MB (94.09%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int n = trips.size();
-        
-        
-        sort(trips.begin(), trips.end(), 
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[1] < b[1];
-             });
-        
-       
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            int num = trips[i][0];  
-            int from = trips[i][1]; 
-            int to = trips[i][2];   
-            
-            
-            while (!pq.empty() && pq.top().first <= from) {
-                cnt -= pq.top().second;
+        int n=trips.size();
+        int cnt=0;
+        using P=pair<int,int>;
+        sort(trips.begin(),trips.end(),[](vector<int>& a,vector<int>& b){
+            if(a[1]==b[1]){
+                return a[2]<b[2];
+            }
+            return a[1]<b[1];
+        });
+        priority_queue<P,vector<P>,greater<P>>pq;
+        for(int i=0;i<n;i++){
+            int no=trips[i][0];
+            int u=trips[i][1];
+            int v=trips[i][2];
+            while(!pq.empty() && pq.top().first<=u){
+                cnt-=pq.top().second;
                 pq.pop();
             }
-            
            
-            cnt += num;
-            if (cnt > capacity) return false;
-           
-            pq.push({to, num});
+            if(cnt+no>capacity){
+                return false;
+            }
+            cnt+=no;
+             pq.push({v,no});
         }
         return true;
     }
