4ba40d94e687c2c05d8a54df13dcf5882be33f6e
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-10 02:13:14 +0530

    Time: 64 ms (52.57%) | Memory: 101.5 MB (36.08%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.cpp
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -1,45 +1,54 @@
-struct TrieNode{
-    TrieNode* children[26]={nullptr};
-    vector<string>suggestion;
+class TrieNode {
+    public:
+    TrieNode* children[26];
+    vector<string> suggestion;
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
 };
-
 class Solution {
-public:
-     TrieNode* root;
-     Solution(){
-        root=new TrieNode();
-     }
-     void insert(string& word){
-        TrieNode* node=root;
-        for(auto c: word){
-            int idx=c-'a';
-            if(!node->children[idx]){
-                node->children[idx]=new TrieNode();
+    public:
+    TrieNode* root;
+    Solution() { root = new TrieNode(); }
+    void insert(string& word) {
+        TrieNode* node = root;
+        for (auto ch : word) {
+            if (!node->children[ch - 'a']) {
+                node->children[ch - 'a'] = new TrieNode();
             }
-            node=node->children[idx];
-            if(node->suggestion.size()<3){
+            node = node->children[ch - 'a'];
+            if (node->suggestion.size() < 3) {
                 node->suggestion.push_back(word);
             }
         }
-     }
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(),products.end());
-        vector<vector<string>>res;
-        for(auto& p: products){
-            insert(p);
-        }
-        TrieNode* node=root;
-       for(char& ch:searchWord){
-        if(node){
-         node=node->children[ch-'a'];
-        }
-        if(node){
-            res.push_back(node->suggestion);
-        }else{
-            res.push_back({});
-        }
-       }
-       return res;
+    }
 
+
+    vector<vector<string>> suggestedProducts(vector<string>& products,
+                                             string searchWord) {
+        int n = products.size();
+          sort(products.begin(), products.end());
+        for (int i = 0; i < n; i++) {
+            string ans = products[i];
+            insert(ans);
+        }
+        TrieNode* curr = root;
+        int m = searchWord.size();
+        vector<vector<string>> result;
+        for (int i = 0; i < m; i++) {
+            char ch = searchWord[i];
+            int val = ch - 'a';
+             if (curr)
+                curr = curr->children[val];
+            
+            // ✅ MINOR FIX #2 — if curr is nullptr, push empty vector
+            if (curr)
+                result.push_back(curr->suggestion);
+            else
+                result.push_back({});
+        }
+        return result;
     }
 };
