37aa3dc60e2b521323ae9bd2108d5b0b338de6ee
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-11 17:15:50 +0530

    Time: 105 ms (37.47%) | Memory: 99.8 MB (32.30%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,30 +1,29 @@
 class Solution {
-private:
-    vector<vector<int>> dp;
-
-    int solve(int idx, vector<int>& prices, int fee, int pur) {
-        int n = prices.size();
-        if (idx == n) return 0;  
-
-        if (dp[idx][pur] != -1) return dp[idx][pur];
-
-        int take = 0;
-        if (pur) {
-          
-            take = -prices[idx] + solve(idx + 1, prices, fee, 0);
-        } else {
-           
-            take = prices[idx] - fee + solve(idx + 1, prices, fee, 1);
+    private:
+    vector<vector<int>>dp;
+    int solve(int idx,vector<int>& prices,int fee,int hold){
+        int n=prices.size();
+        if(idx==n){
+            return 0;
         }
+        if(dp[idx][hold]!=-1){
+            return dp[idx][hold];
+        }
+        int skip=solve(idx+1,prices,fee,hold);
+         if(hold==1){
+            int take=prices[idx]-fee+solve(idx+1,prices,fee,0);
+            return dp[idx][hold]= max(take,skip);
+         }else{
+            int take=-prices[idx]+solve(idx+1,prices,fee,1);
+            return dp[idx][hold]= max(take,skip);
+         }
 
-        int skip = solve(idx + 1, prices, fee, pur);
-        return dp[idx][pur] = max(take, skip);
     }
-
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        dp.assign(n, vector<int>(2, -1));
-        return solve(0, prices, fee, 1); 
+        int n=prices.size();
+        int hold=0;
+        dp.resize(n+1,vector<int>(3,-1));
+        return solve(0,prices,fee,hold);
     }
 };
