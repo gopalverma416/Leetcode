163d791f3cb987b2b3a5cc2409067f80b66496db
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-12 22:51:10 +0530

    Time: 4 ms (44.39%) | Memory: 19.2 MB (20.84%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -1,27 +1,35 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 private:
-    vector<TreeNode*> solve(int start, int end) {
-        vector<TreeNode*> ans;
 
-       
+    vector<TreeNode*> solve(int start, int end) {
+          vector<TreeNode*>ans;
         if (start > end) {
             ans.push_back(nullptr);
             return ans;
         }
-
-        
+      
         for (int i = start; i <= end; i++) {
             vector<TreeNode*> leftTree = solve(start, i - 1);
             vector<TreeNode*> rightTree = solve(i + 1, end);
 
-            
-            for (auto left : leftTree) {
-                for (auto right : rightTree) {
-                    TreeNode* root = new TreeNode(i);
-                    root->left = left;
-                    root->right = right;
-                    ans.push_back(root);
+            for(auto left: leftTree){
+                for( auto right: rightTree){
+                    TreeNode* root=new TreeNode(i);
+                    root->left=left;
+                    root->right=right;
+                   ans.push_back(root);
                 }
             }
         }
@@ -29,8 +37,5 @@ private:
     }
 
 public:
-    vector<TreeNode*> generateTrees(int n) {
-        if (n == 0) return {};    
-        return solve(1, n);         
-    }
+    vector<TreeNode*> generateTrees(int n) { return solve(1, n); }
 };
