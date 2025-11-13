286e2b83f8750b75f68fe22b48fc6478388d167f
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-13 15:45:38 +0530

    Time: 79 ms (75.44%) | Memory: 44.3 MB (68.41%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1);  // 1-based index
-        for (auto& t : times) {
+
+        vector<vector<pair<int,int>>> adj(n+1);
+        for (auto &t : times) {
             int u = t[0], v = t[1], w = t[2];
-            adj[u].emplace_back(v, w);
+            adj[u].push_back({v, w}); // directed + weighted
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        pq.emplace(0, k);
+        using P = pair<int,int>; // dist, node
+        priority_queue<P, vector<P>, greater<P>> pq;
 
-        vector<int> dist(n + 1, INT_MAX);
+        vector<int> dist(n+1, INT_MAX);
         dist[k] = 0;
+        pq.push({0, k});
 
         while (!pq.empty()) {
             auto [d, node] = pq.top();
             pq.pop();
 
-            // Skip if we've already found a better path
             if (d > dist[node]) continue;
 
-            for (auto& [next, weight] : adj[node]) {
-                if (d + weight < dist[next]) {
-                    dist[next] = d + weight;
-                    pq.emplace(dist[next], next);
+            for (auto &it : adj[node]) {
+                int nxt = it.first;
+                int w = it.second;
+
+                if (dist[nxt] > d + w) {
+                    dist[nxt] = d + w;
+                    pq.push({dist[nxt], nxt});
                 }
             }
         }
 
-        int maxTime = 0;
+        int ans = 0;
         for (int i = 1; i <= n; i++) {
             if (dist[i] == INT_MAX) return -1;
-            maxTime = max(maxTime, dist[i]);
+            ans = max(ans, dist[i]);
         }
 
-        return maxTime;
+        return ans;
     }
 };
