d5924822179bcdd3c2109dbc9e9126fd070c8650
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-13 14:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (71.10%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,58 +1,59 @@
 class Solution {
-    //second step coplemetd
-    private:
-    vector<int>parent;
-    vector<int>rank;
-    int findParent(int node){
-        if(node==parent[node]){
-            return node;
-        }
-        return parent[node]=findParent(parent[node]);
-    }
-    void unionFind(int u,int v){
-        int u_parent=findParent(u);
-        int v_parent=findParent(v);
-        if(u_parent==v_parent){
-            return;
-        }
-        if(rank[u_parent]>rank[v_parent]){
-            parent[v_parent]=u_parent;
-          rank[u]+=1;
-        }else if(rank[u_parent]<rank[v_parent]){
-           parent[u_parent]=v_parent;
-           rank[v_parent]+=1;
-        }else{
-             parent[u_parent]=v_parent;
-             rank[u_parent]+=1;
-        }
-    }
-    
-
 public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-      int m=connections.size();
-      if(n>m+1){
-        return -1;
-      }
-      parent.resize(n+1);
-      for(int i=0;i<n;i++){
-      parent[i]=i;
-      }
-     //first step completed 
-     
-     rank.resize(n+1,1);
-  for(auto it: connections){
-    int u=it[0];
-    int v=it[1];
-    unionFind(u,v);
-  }
-   int component=0;
-    for(int i=0;i<parent.size();i++){
-        if(parent[i]==i){
-           component++;
+    class Dsu {
+        public:
+        vector<int> size;
+        vector<int> parent;
+        Dsu(int n) {
+            parent.resize(n,0);
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+            size.resize(n, 1);
         }
-    }
-    return component-1;
+        int findPar(int u) {
+            if (u == parent[u]) {
+                return u;
+            }
+            return parent[u] = findPar(parent[u]);
+        }
+        void insert(int u, int v) {
+            int ult_u = findPar(u);
+            int ult_v = findPar(v);
+            if (size[ult_u] > size[ult_v]) {
+                parent[ult_v] = ult_u;
+                size[ult_u] += size[ult_v];
+            } else {
+                parent[ult_u] = ult_v;
+                size[ult_v] += size[ult_u];
+            }
+        }
+    };
 
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        Dsu dsu(n);
+        int sz = connections.size();
+        for (int i = 0; i < sz; i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
+            int ult_u = dsu.findPar(u);
+            int ult_v = dsu.findPar(v);
+            if (ult_u != ult_v) {
+                dsu.insert(u, v);
+            }
+        }
+        vector<int>arr=dsu.parent;
+        int need=0;
+        for(int i=0;i<arr.size();i++){
+          if(arr[i]==i){
+            need++;
+          }
+        }
+        need--;
+        if(sz<n-1){
+            return -1;
+        }
+        return need;
     }
 };
