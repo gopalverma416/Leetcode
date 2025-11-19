da814efe4f86d58770c8daaa57d79a18ceef54f0
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-19 19:39:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,37 +1,60 @@
 class Solution {
-private:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[node] = 1;
-        for (auto neig : adj[node]) {
-            if (vis[neig] == 1) {
-                continue;
-            }
-            dfs(neig, adj, vis);
-        }
-    }
-
 public:
-    int findCircleNum(vector<vector<int>>& nums) {
+    class Dsu {
+        public:
+             vector<int> par;
+            vector<int> rank;
+        Dsu(int n) {
+           
+            par.resize(n, 0);
+            rank.resize(n, 1);
+            for (int i = 0; i < n; i++) {
+                par[i] = i;
+            }
+        }
+
+        int findPar(int u) {
+            if (u == par[u]) {
+                return u;
+            }
+            return par[u] = findPar(par[u]);
+        }
+
+        void unionA(int u, int v) {
+            int ult_u = findPar(u);
+            int ult_v = findPar(v);
+            if (rank[ult_u] > rank[ult_v]) {
+                par[ult_v] = ult_u;
+            } else if (rank[ult_u] < rank[ult_v]) {
+                par[ult_u] = ult_v;
+            } else {
+                par[ult_v] = ult_u;
+                rank[ult_u]++;
+            }
+        }
+    };
+
+    int
+    findCircleNum(vector<vector<int>>& nums) {
         int n = nums.size();
-        vector<vector<int>> adj(n + 1);
+        Dsu dsu(n);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < nums[i].size(); j++) {
-                int u = i;
-                int v = j;
-             
-                if (v == u) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || nums[i][j] == 0) {
                     continue;
                 }
-                if(nums[i][j]==1){
-                  adj[u].push_back(v);
+                int ult_u = dsu.findPar(i);
+                int ult_v = dsu.findPar(j);
+                if (ult_u == ult_v) {
+                    continue;
                 }
+                dsu.unionA(i, j);
             }
         }
         int cnt = 0;
-        vector<int> vis(n + 1, -1);
         for (int i = 0; i < n; i++) {
-            if (vis[i] == -1) {
-                dfs(i, adj, vis);
+            int ult_u = dsu.findPar(i);
+            if (ult_u == i) {
                 cnt++;
             }
         }
