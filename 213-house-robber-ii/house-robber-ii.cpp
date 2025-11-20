4cb5e4ca1d375ff83d4e73ce58145cb317c0df39
Gopal verma <141685483+gopalverma416@users.noreply.github.com>
2025-11-20 16:50:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (38.75%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
-private:
-    int solve(int start, int end, vector<int>& nums, vector<int>& dp) {
-        if (start > end) return 0;
-        if (dp[start] != -1) return dp[start];
-
+    private:
+    int solve(int idx,vector<int>& nums,int right,vector<int>& dp){
        
-        int take = nums[start] + solve(start + 2, end, nums, dp);
-       
-        int skip = solve(start + 1, end, nums, dp);
-
-        return dp[start] = max(take, skip);
+        if(idx==right){
+            return nums[idx];
+        }
+        if(idx>=(right+1)){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int take=nums[idx]+solve(idx+2,nums,right,dp);
+        int drop=solve(idx+1,nums,right,dp);
+        return dp[idx]= max(take,drop);
     }
-
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
-
-        vector<int> dp1(n, -1), dp2(n, -1);
-        int first = solve(0, n - 2, nums, dp1); 
-        int second = solve(1, n - 1, nums, dp2); 
-
-        return max(first, second);
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+         vector<int>dp(n,-1);
+        int take1= solve(0,nums,n-2,dp);
+        dp.assign(n, -1);
+        int take2=solve(1,nums,n-1,dp);
+        return max(take1,take2);
     }
 };
